1102606a8bc75d696eab1d7d146470bffbb4c2d5
Koyama, Toshiyuki <Toshiyuki.Koyama@arcserve.com>
2018-05-10 17:24:19 +0900

    comment out VixDiskLib_EndAccess to avoid hang

--- doc/sample/vixDiskLibSample.cpp
+++ doc/sample/vixDiskLibSample.cpp
@@ -706,7 +706,7 @@ public:
 
 			printf("\n call VixDiskLib_Close now\n");
            VixError vixError = VixDiskLib_Close(_handle);
-		   printf("Disk[%d] is closed. vixError: %llx\n", _id, vixError);
+		   printf("Disk[%d] is closed. vixError: %llx\n", _id, vixError);   
         }
         _info = NULL;
         _handle = NULL;
@@ -1289,9 +1289,9 @@ main(int argc, char* argv[])
        retval = 1;
     }
 
-    if (appGlobals.vmxSpec != NULL) {
-       vixError = VixDiskLib_EndAccess(&cnxParams, "Sample");
-    }
+    //if (appGlobals.vmxSpec != NULL) {
+    //   vixError = VixDiskLib_EndAccess(&cnxParams, "Sample");
+    //}
     if (appGlobals.connection != NULL) {
        VixDiskLib_Disconnect(appGlobals.connection);
     }
